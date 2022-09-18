07652914135f6e627084b47b845b71986625b89a
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-18 17:33:32 +0300

    c

---
+++ src/app/Models/Resource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+
+class Resource extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $table = 'resource';
+
+    public function seoquery(): MorphMany
+    {
+        return $this->morphMany(Seoquery::class, 'parentable');
+    }
+
+    public function parentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
